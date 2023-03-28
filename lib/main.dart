f283576e73ea13825cd6441b2f5bd0b054697be2
Mitzy Mo <mitzy.93@gmail.com>
2023-03-27 21:34:02 -0500

    "adding menu for favorites and home page"

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,15 @@ class MyAppState extends ChangeNotifier {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+//Underscore in the name of a class, means private
+class _MyHomePageState extends State<MyHomePage> {
+  var selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,9 +71,10 @@ class MyHomePage extends StatelessWidget {
                   label: Text('Favorites'),
                 ),
               ],
-              selectedIndex: 0,
+              selectedIndex: selectedIndex,
               onDestinationSelected: (value) {
-                print('selected: $value');
+                setState(() {});
+                selectedIndex = value;
               },
             ),
           ),
